b758ba3b511d6088bcae947e0a1bde3041f76fc1
workus-kmj <kmj@workus.click>
2025-11-23 00:14:43 +0900

    프론트엔드 연결

---
+++ src/main/java/com/workus/workus/common/controller/TimeController.java
@@ -0,0 +1,19 @@
+package com.workus.workus.common.controller;
+
+import java.time.ZonedDateTime;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TimeController {
+
+	@GetMapping("/time")
+	public Map<String, String> getServerTime() {
+		ZonedDateTime now = ZonedDateTime.now();
+		return Map.of("serverTime", now.toString());
+	}
+}
