25fa179306a62088c2341a0a9ac95b3c03087de7
workus-admin <workus@workus.click>
2025-11-23 18:13:02 +0900

    Revert "Workus/31 머지테스트"

--- src/main/java/com/workus/workus/common/controller/TimeController.java
+++ src/main/java/com/workus/workus/common/controller/TimeController.java
@@ -3,21 +3,17 @@ package com.workus.workus.common.controller;
 import java.time.ZonedDateTime;
 import java.util.Map;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-@Slf4j
 public class TimeController {
 
 	@GetMapping("/time")
 	public Map<String, String> getServerTime() {
 		ZonedDateTime now = ZonedDateTime.now();
-        log.info(now.toString());
-        log.info(Thread.currentThread().getStackTrace()[0].getMethodName());
 		return Map.of("serverTime", now.toString());
 	}
 }
