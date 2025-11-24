2bdbed0e22b4048add5229722abf9b5c3c3e8f07
workus-lys <lys@workus.click>
2025-11-24 18:35:01 +0900

    #workus/28 기초 DB 스크립트 작성

--- src/main/java/com/workus/workus/common/controller/TimeController.java
+++ src/main/java/com/workus/workus/common/controller/TimeController.java
@@ -3,12 +3,14 @@ package com.workus.workus.common.controller;
 import java.time.ZonedDateTime;
 import java.util.Map;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@Slf4j
 public class TimeController {
 
 	@GetMapping("/time")
