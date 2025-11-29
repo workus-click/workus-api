5d079f10ff04fdcbd617acc2cc730c1b6d4259c2
workus-lys <lys@workus.click>
2025-11-29 23:32:17 +0900

    #workus/42 테스트시 test프로파일로 자동 지정

--- src/test/java/com/workus/workus/WorkusApplicationTests.java
+++ src/test/java/com/workus/workus/WorkusApplicationTests.java
@@ -1,13 +1,24 @@
 package com.workus.workus;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-@ActiveProfiles("test")
-class WorkusApplicationTests {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@SpringBootTest
+class WorkusApplicationTests {
+    @Autowired
+    Environment environment;
+
+    @Test
+    void checkActiveProfileIsTest() {
+        String[] activeProfiles = environment.getActiveProfiles();
+        assertEquals(1, activeProfiles.length);
+        assertEquals("test", activeProfiles[0]);
+    }
 	@Test
 	void contextLoads() {
 	}
