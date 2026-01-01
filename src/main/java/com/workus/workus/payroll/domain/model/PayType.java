c44f0e29e787bf6ff392e4e33e1cbcacae8129a1
sobee <sobee1403@daum.net>
2026-01-02 01:14:27 +0900

    workus/56 근무시간설정 도메인 개발건 빌드 실패에 따른 수정

---
+++ src/main/java/com/workus/workus/payroll/domain/model/PayType.java
@@ -0,0 +1,20 @@
+package com.workus.workus.payroll.domain.model;
+
+public enum PayType {
+    HOURLY("시급"),
+    DAILY("일급"),        // 현재 미사용
+    MONTHLY("월급"),
+    ANNUAL("연봉");        // 현재 미사용
+
+    private final String description;
+
+    PayType(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+}
+
